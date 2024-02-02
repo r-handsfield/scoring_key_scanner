dcf788c3a756246a90ecb7fcf7498c8fb2d0e398
RH <r.handsfield@gmail.com>
2024-02-02 02:36:23 -0500

    Completes category mark location and conversion to python dictionary.

--- src/exp_6.py
+++ src/exp_6.py
@@ -15,6 +15,9 @@ from imutils.contours import sort_contours
 sys.path.append('classes')
 
 from scoreKey import ScoreKey
+from sheetUtilities import SheetUtilities
+
+util = SheetUtilities()
 
 PATH_E = abspath("./images/ske.pdf")
 PATH_M = abspath("./images/skm.pdf")
@@ -65,11 +68,11 @@ for q in range(38):
 
 # Close contours to improve line detection
 kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (5,1))
-gray = cv2.morphologyEx(gray, cv2.MORPH_CLOSE, kernel)
-bin = cv2.threshold(gray, 250, 255, cv2.THRESH_BINARY)[1]
-inv = cv2.threshold(gray, 250, 255, cv2.THRESH_BINARY_INV)[1]
+closed = cv2.morphologyEx(gray, cv2.MORPH_CLOSE, kernel)
+closed_bin = cv2.threshold(closed, 250, 255, cv2.THRESH_BINARY)[1]
+closed_inv = cv2.threshold(closed, 250, 255, cv2.THRESH_BINARY_INV)[1]
 
-contours = cv2.findContours(inv, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)[0]
+contours = cv2.findContours(closed_inv, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)[0]
 candidates = list(contours)
 
 
@@ -93,10 +96,70 @@ def filter_fcn(contour):
 lines = list(filter(filter_fcn, list(contours)))
 print(len(lines))
 
-bin = cv2.cvtColor(bin, cv2.COLOR_GRAY2BGR)
-pic = bin.copy()
+pic = gray.copy()
+pic = cv2.cvtColor(pic, cv2.COLOR_GRAY2BGR)
 cv2.drawContours(pic, lines, -1, (0,0,255), 1)
 cv2.imshow("Contours", pic)
 
-if cv2.waitKey(0) == 27:  # Esc will kill the display loop
-    cv2.destroyAllWindows()
+cv2.waitKey(0)
+# cv2.destroyAllWindows()
+# if cv2.waitKey(0) == 27:  # Esc will kill the display loop
+#     cv2.destroyAllWindows()
+
+marks = {}
+for i, l in enumerate(lines):
+    x,y,w,h = cv2.boundingRect(l)
+    area = w*h
+    aspect = float(w)/h
+
+    marks[i] = {'x':x, 'y':y, 'w':w, 'h':h, 'area':area, 'aspect':aspect}
+
+df_contours = pd.DataFrame(data=marks).T
+print(df_contours)
+col_x = sorted(df_contours.x.unique())
+row_y = sorted(df_contours.y.unique())
+print(col_x)
+
+if len(col_x) > 7:
+    for i in range(1, len(col_x)):
+        prev, curr = col_x[i-1], col_x[i]
+
+        # Collapse values that are within 5px of each other
+        if util.close_to(prev, curr, 5):
+             col_x[i] = prev       
+
+col_x = pd.Series(col_x).unique()
+print(col_x)
+
+if len(row_y) != 30:
+    raise ValueError(f"There shold be 30 rows, not {len(row_y)}.")
+
+# Create dict of x-values and column names for indexing into the Key df
+col_labels = ['N', 'A', 'F', 'G', 'S', 'IES', 'MDL']
+col_index =  dict(zip(col_x, col_labels))
+# print(col_index)
+
+# df = pd.DataFrame(index=range(1,31), columns=columns)
+# df = pd.DataFrame(data=np.zeros((30, 7), dtype='bool'), index=range(1,31), columns=['N', 'A', 'F', 'G', 'S', 'IES', 'MDL'])
+df_scorekey = pd.DataFrame(data=np.zeros((30, 7), dtype='bool'), index=range(1,31), columns=col_labels)
+# print(df_scorekey)
+
+
+for i in range(len(df_contours)):
+    x, y = df_contours.loc[i, 'x'], df_contours.loc[i, 'y']
+    row = 1 + row_y.index(y) 
+    x = min(col_x, key=lambda el:abs(el-x)) # Align x to closest unique value
+    col = col_index[x]
+    # print(i, x, y, row, col)
+    df_scorekey.loc[row, col] = True
+
+print(df_scorekey)
+
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+score_key = df_scorekey.T.to_dict()
+for k, v in score_key.items():
+    print(k, ':', v)
+
