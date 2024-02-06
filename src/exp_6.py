3840f37ee30cc48f312c8da8b2c49fac6d1b32c9
RH <r.handsfield@gmail.com>
2024-02-06 11:51:17 -0500

    Segments the dataframe version into its own code section.

--- src/exp_6.py
+++ src/exp_6.py
@@ -67,6 +67,7 @@ for q in range(38):
 
 
 # Close contours to improve line detection
+# Kernel is a horizontal line (cols, rows)
 kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (5,1))
 closed = cv2.morphologyEx(gray, cv2.MORPH_CLOSE, kernel)
 closed_bin = cv2.threshold(closed, 250, 255, cv2.THRESH_BINARY)[1]
@@ -76,7 +77,7 @@ contours = cv2.findContours(closed_inv, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)[
 candidates = list(contours)
 
 
-### Filter using a function
+### Filter the marker lines using a function
 def filter_fcn(contour):
     c = contour
     x,y,w,h = cv2.boundingRect(c)
@@ -93,19 +94,21 @@ def filter_fcn(contour):
     else:
         return True
     
+# the marker lines
 lines = list(filter(filter_fcn, list(contours)))
 print(len(lines))
 
 pic = gray.copy()
 pic = cv2.cvtColor(pic, cv2.COLOR_GRAY2BGR)
 cv2.drawContours(pic, lines, -1, (0,0,255), 1)
-cv2.imshow("Contours", pic)
+cv2.imshow("Marker Lines", pic)
 
 cv2.waitKey(0)
 # cv2.destroyAllWindows()
 # if cv2.waitKey(0) == 27:  # Esc will kill the display loop
 #     cv2.destroyAllWindows()
 
+# Build dict of marker line contour attributes
 marks = {}
 for i, l in enumerate(lines):
     x,y,w,h = cv2.boundingRect(l)
@@ -114,52 +117,63 @@ for i, l in enumerate(lines):
 
     marks[i] = {'x':x, 'y':y, 'w':w, 'h':h, 'area':area, 'aspect':aspect}
 
-df_contours = pd.DataFrame(data=marks).T
-print(df_contours)
-col_x = sorted(df_contours.x.unique())
-row_y = sorted(df_contours.y.unique())
-print(col_x)
+### DataFrame Method ######################################################yy
+# 
+# This version builds dataframes and interates through them to match the
+# marker position to a question and category.
 
-if len(col_x) > 7:
-    for i in range(1, len(col_x)):
-        prev, curr = col_x[i-1], col_x[i]
+# Use the dict to build a dataframe for easy viewing & operations
+df_contours = pd.DataFrame(data=marks).T
+print(df_contours, '', sep='\n')
+
+# Get unique x, y valued of marker lines
+unique_x = sorted(df_contours.x.unique()) 
+unique_y = sorted(df_contours.y.unique())
+print("Unique x coordinates", unique_x, sep='\n')
+
+if len(unique_x) > 7:
+    for i in range(1, len(unique_x)):
+        prev, curr = unique_x[i-1], unique_x[i]
 
         # Collapse values that are within 5px of each other
         if util.close_to(prev, curr, 5):
-             col_x[i] = prev       
+             unique_x[i] = prev       
 
-col_x = pd.Series(col_x).unique()
-print(col_x)
+unique_x = pd.Series(unique_x).unique()
+print(unique_x, '', sep='\n')
 
-if len(row_y) != 30:
-    raise ValueError(f"There shold be 30 rows, not {len(row_y)}.")
+if len(unique_y) != 30:
+    raise ValueError(f"There shold be 30 rows, not {len(unique_y)}.")
 
 # Create dict of x-values and column names for indexing into the Key df
 col_labels = ['N', 'A', 'F', 'G', 'S', 'IES', 'MDL']
-col_index =  dict(zip(col_x, col_labels))
-# print(col_index)
+col_index =  dict(zip(unique_x, col_labels))
 
-# df = pd.DataFrame(index=range(1,31), columns=columns)
-# df = pd.DataFrame(data=np.zeros((30, 7), dtype='bool'), index=range(1,31), columns=['N', 'A', 'F', 'G', 'S', 'IES', 'MDL'])
+# Initialize dataframe to hold category data
 df_scorekey = pd.DataFrame(data=np.zeros((30, 7), dtype='bool'), index=range(1,31), columns=col_labels)
-# print(df_scorekey)
 
-
+# Fill dataframe with category data based on position of marker lines.
 for i in range(len(df_contours)):
     x, y = df_contours.loc[i, 'x'], df_contours.loc[i, 'y']
-    row = 1 + row_y.index(y) 
-    x = min(col_x, key=lambda el:abs(el-x)) # Align x to closest unique value
+    row = 1 + unique_y.index(y) 
+    x = min(unique_x, key=lambda el:abs(el-x)) # Align x to closest unique value
     col = col_index[x]
     # print(i, x, y, row, col)
     df_scorekey.loc[row, col] = True
+print("Category Dataframe", df_scorekey, '', sep='\n')
 
-print(df_scorekey)
-
-
-cv2.waitKey(0)
-cv2.destroyAllWindows()
-
+# Convert dataframe to dict just to test how it's done
+print("Caetgory Dict")
 score_key = df_scorekey.T.to_dict()
 for k, v in score_key.items():
     print(k, ':', v)
 
+### END DataFrame Method ######################################################yy
+
+###
+### Dict Method #####################################################
+# 
+# This version skips the dataframes and constructs indexing dicts that
+# are used to match the marker's position with the questions and category
+
+marker_lines = marks  # dict of marker line contour attributes
